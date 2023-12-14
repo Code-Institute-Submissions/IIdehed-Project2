1dce047f80f2e7e569326aa3fa4b51f75a2bb478
Idehed <>
2023-12-14 17:42:39 +0000

    Adding questions in anther js file

---
+++ assets/js/questions.js
@@ -0,0 +1,131 @@
+const questions = [
+
+    /* Question 1 */
+
+    {
+        question: "What is the name of the killer clown in Stephen King's IT?",
+        answers: [
+        { text: 'Bozo', correct: false},
+        { text: 'Captain Spaulding', correct: false },
+        { text: 'Pennywise', correct: true },
+        { text: 'Mr. Mime', correct: false },
+        ]
+    }, 
+
+    /* Question 2 */
+
+    {
+        question: "What is the name of the main villain in the original 'Texas Chainsaw Massacre'?",
+        answers: [
+            { text: 'Leatherface', correct: true },
+            { text: 'Pinhead', correct: false },
+            { text: 'Ghostface', correct: false },
+            { text: 'Slicey McStabby', correct: false },
+        ]
+    }, 
+
+    /* Question 3 */
+
+    {
+        question: "Here's Johnny! What is the name of the hotel where 'The Shining' takes place?",
+        answers: [
+            { text: 'The Grand Frontier', correct: false },
+            { text: 'Overwatch Hotel', correct: false },
+            { text: 'Glacier Peak', correct: false },
+            { text: 'Overlook Hotel', correct: true },
+        ]
+    }, 
+
+    /* Question 4 */
+
+    {
+        question: "What was the name of the infamous fishing boat used in the climatic shark hunt at the end of 'JAWS'?",
+        answers: [
+            { text: 'Spellbound', correct: false },
+            { text: 'Orca', correct: true },
+            { text: 'High Tide', correct: false },
+            { text: 'Buoy O Buoy', correct: false },
+        ]
+    }, 
+
+    /* Question 5 */
+
+    {
+        question: "How does the (mostly) unseen monster from 'It Follows' decide who to target?",
+        answers: [
+            { text: 'Sexual partners', correct: true },
+            { text: 'Virgins', correct: false },
+            { text: 'Atheists', correct: false },
+            { text: 'Females Only', correct: false },
+        ]
+    },
+
+    /* Question 6 */
+
+    {
+        question: "Who was the main protagonist in the 'Scream' series?",
+        answers: [
+            { text: 'Dewey Riley', correct: false },
+            { text: 'Sidney Prescott', correct: true },
+            { text: 'Casey Becker', correct: false },
+            { text: 'Cotton Weary', correct: false },
+        ]
+    },
+
+    /* Question 7 */
+
+    {
+        question: "Before he was caught and burned to death by angry townspeople, Freddy Krueger was a serial killer known as the _________________ ?",
+        answers: [
+            { text: 'Elm Street Murderer', correct: false },
+            { text: 'Ohio State Stabber', correct: false },
+            { text: 'Springwood Slasher', correct: true },
+            { text: 'Kiddy Killer', correct: false },
+        ]
+    },
+
+    /* Question 8 */
+
+    {
+        question: "What is the name of the mysterious hypnotic location that Chris finds himself in during 'Get Out'?",
+        answers: [
+            { text: 'The tea cup', correct: false },
+            { text: 'The sunken place', correct: true },
+            { text: 'The apple orchard', correct: false },
+            { text: 'The alternate world', correct: false },
+        ]
+    },
+
+    /* Question 9 */
+
+    {
+        question: "The Purge is about a 24-hour period where all crime is legal in America. How often does this happen?",
+        answers: [
+            { text: 'Once every six months', correct: false },
+            { text: 'Once a month', correct: false },
+            { text: 'Once every five years', correct: false },
+            { text: 'Once a year', correct: true },
+        ]
+    },
+
+    /* Question 10 */
+
+    {
+        question: "Michael Myers' mask is actually a mask for what 'Star Trek' character?",
+        answers: [
+            { text: 'Mr. Spock', correct: false },
+            { text: 'Leonard McCoy', correct: false },
+            { text: 'Jean Luc-Picard', correct: false },
+            { text: 'James T. Kirk', correct: true },
+        ]
+    },
+
+
+    
+
+
+
+
+
+]
+
