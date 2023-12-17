40985077866895db1eca18e5fa38ab9128af12ab
Idehed <>
2023-12-17 13:00:35 +0000

    adding some stylesfor levels.container and creating active sections in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
 /* Get all the required elements */
 const infoContainer = document.querySelector(".quiz-info-container ");
 const startButton = document.querySelector(".start-btn button");
+const continueBtn = document.querySelector(".buttons .continuebtn")
 const levelsContainer = document.querySelector(".levels-container")
 const quizContainer = document.querySelector(".quiz-game-container");
 const playAgainbutton = document.querySelector(".btn .play-again-btn");
 const scoreSection = document.querySelector("score-section");
+
+
 
 
 /* Show Level section if startbutton is pressed */
@@ -12,3 +15,8 @@ startButton.onclick = () => {
     infoContainer.classList.add("activeInfo"); /* Show level section if pressed */
 }
 
+
+continueBtn.onclick = () => {
+    infoContainer.classList.remove("activeInfo"); /* hide the info section */
+    quizContainer.classList.add("activeQ"); /* Show the quiz section */
+}
