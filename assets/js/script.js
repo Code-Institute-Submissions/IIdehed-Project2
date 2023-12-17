6508d25aeeb111fb3f88e75426b20b7ed167cc86
Idehed <>
2023-12-17 20:15:06 +0000

    added a disabel class if an option is pressed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 /* Get all the required elements */
 const infoContainer = document.querySelector(".quiz-info-container ");
 const startButton = document.querySelector(".start-btn button");
-const continueBtn = document.querySelector(".buttons .continuebtn")
+const continueBtn = document.querySelector(".buttons .continuebtn");
 const quizContainer = document.querySelector(".quiz-game-container");
 const playAgainbutton = document.querySelector(".btn .play-again-btn");
 const scoreSection = document.querySelector("score-section");
+const OptionsList = document.querySelector(".answers-options-list");
 
 
 
@@ -12,15 +13,15 @@ const scoreSection = document.querySelector("score-section");
 /* Show Level section if startbutton is pressed */
 startButton.onclick = () => {
     infoContainer.classList.add("activeInfo"); /* Show level section if pressed */
-}
+};
 
 
 continueBtn.onclick = () => {
     infoContainer.classList.remove("activeInfo"); /* hide the info section */
     quizContainer.classList.add("activeQuiz"); /* Show the quiz section */
     getQuestions(0);
-    questioncounter(1);
-}
+    questionCounter(1);
+};
 
 let questionCount = 0;
 let questionNumb = 1;
@@ -30,32 +31,31 @@ const nextButton = quizContainer.querySelector(".next-button");
 
 /* If next button is pressed */
 nextButton.onclick = () => {
-    if (questionCount < questions.length - 1 ) {
+    if (questionCount < questions.length - 1) {
         questionCount++;
         questionNumb++;
         getQuestions(questionCount);
-        questioncounter(questionNumb);
+        questionCounter(questionNumb);
     }
     else {
         console.log("Questions Completed!");
     }
-}
+};
 
 /* Getting questions and options*/
-function getQuestions (index){
+function getQuestions(index) {
     const questionText = document.querySelector(".question-text");
-    const OptionsList = document.querySelector(".answers-options-list");
-    let questionName = "<span>" + questions[index].numb + ". " + questions[index].question +"</span>";
-    let OptionsText = '<div class="answers">'+ questions[index].options[0]+'<span></span></div>'
-        +  '<div class="answers">'+ questions[index].options[1]+'<span></span></div>'
-        +  '<div class="answers">'+ questions[index].options[2]+'<span></span></div>'
-        +  '<div class="answers">'+ questions[index].options[3]+'<span></span></div>';
+    let questionName = "<span>" + questions[index].numb + ". " + questions[index].question + "</span>";
+    let OptionsText = '<div class="answers">' + questions[index].options[0] + '<span></span></div>'
+        + '<div class="answers">' + questions[index].options[1] + '<span></span></div>'
+        + '<div class="answers">' + questions[index].options[2] + '<span></span></div>'
+        + '<div class="answers">' + questions[index].options[3] + '<span></span></div>';
     questionText.innerHTML = questionName;
     OptionsList.innerHTML = OptionsText;
     const answers = OptionsList.querySelectorAll(".answers");
     for (let i = 0; i < answers.length; i++) {
         answers[i].setAttribute("onclick", "answersSelected(this)");
-        
+
     }
 }
 
@@ -63,21 +63,33 @@ function getQuestions (index){
 function answersSelected(answer) {
     let UserAnswer = answer.textContent;
     let correctAnswer = questions[questionCount].answer;
+    let allOptions = OptionsList.children.length;
     if (UserAnswer == correctAnswer) {
         console.log("Answer is Correct!");
         answer.classList.add("correct");
     }
-    else{
+    else {
         answer.classList.add("incorrect");
         console.log("Answer is Wrong!");
+
+        for (let i = 0; i < allOptions; i++) {
+            if (OptionsList.children[i].textContent == correctAnswer) {
+                OptionsList.children[i].setAttribute("class", "answers correct");
+            }
+        }
+
+        /* Disabel all options if user pressed an answer */
+        for (let i = 0; i < allOptions; i++) {
+            OptionsList.children[i].classList.add("disabled");
+
+        }
+
+
     }
-    
-}
 
 
-function questioncounter(index){
+function questionCounter(index) {
     const bottomCounter = quizContainer.querySelector(".total-questions");
     let totalQuestionText = '<span><p>' + index + '</p>of<p>' + questions.length + '</p>Questions</span>';
     bottomCounter.innerHTML = totalQuestionText;
 }
-
